2df908423cfd2d87cc1bbabfa26460c5bc8e5d4a
Yevhen <shershen.yevhen@gmai>
2022-08-06 20:18:13 +0200

    create header, body and footer structure

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,84 @@
 <template>
   <v-app>
-    <v-card width="400" class="mt-5 mx-auto">
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            label="Username"
-            prepend-icon="mdi-account-circle"
-          ></v-text-field>
-          <v-text-field
-            :type="showPassword ? 'test' : 'password'"
-            label="Password"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye': 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          ></v-text-field>
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-app-bar app color="primary" dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text rounded color="blue lighten-4"> Login </v-btn>
+
+      <v-btn text rounded color="blue lighten-4"> Home </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-content>
+      <v-card width="400" class="mt-5 mx-auto">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Username"
+              prepend-icon="mdi-account-circle"
+            ></v-text-field>
+            <v-text-field
+              :type="showPassword ? 'test' : 'password'"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+      <v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-1 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Footer App</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
@@ -36,7 +89,8 @@ export default Vue.extend({
   name: "App",
 
   data: () => ({
-  showPassword: false
+    showPassword: false,
+    links: ['Login', 'Hello']
   }),
 });
 </script>
