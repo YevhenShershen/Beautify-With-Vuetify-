7f71411f6c395d5d58474d8e7022a43bbbeb1958
Yevhen <shershen.yevhen@gmai>
2022-08-12 10:24:44 +0200

    Forms - Validation

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,10 +1,15 @@
 <template>
   <v-main>
     <v-row>
-      <v-col  class="mx-3">
+      <v-col class="mx-3">
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="Email" type="email"></v-text-field>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+          ></v-text-field>
           <v-autocomplete
             label="Which browser do you use?"
             :items="browsers"
@@ -16,8 +21,18 @@
             readonly
           ></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
+          <v-btn type="submit" color="primary" class="mr-4" :disabled="!formValidity">Submit</v-btn>
+          <v-btn type="submit" color="success" class="mr-4" @click="validateForm">Validate Form</v-btn>
+          <v-btn color="warning" @click="resetValidation" class="mr-4"
+            >Reset Validation</v-btn
+          >
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -27,8 +42,39 @@
 <script>
 export default {
   data: () => ({
+    agreeToTerms: false,
+    agreeToTermsRules: [
+      (value) =>
+        !!value ||
+        "You must agree to the terms and conditions to sign up for an account",
+    ],
     browsers: ["Chrome", "Firefox", "Safari", "Edge"],
     birthday: "",
+    email: "",
+    emailRules: [
+      (value) => !!value || "Email is required.",
+      (value) => value.indexOf("@") !== 0 || "Email should have a user name.",
+      (value) => value.includes("@") || "Email should include an @ symbol.",
+      (value) =>
+        value.indexOf(".") - value.indexOf("@") > 1 ||
+        "Email should contain a valid domain.",
+      (value) =>
+        value.indexOf(".") <= value.length - 3 ||
+        "Email should contain a valid domain extension.",
+    ],
+    formValidity: false
   }),
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset();
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation();
+    },
+    validateForm(){
+      this.$refs.signUpForm.validate()
+    }
+  },
 };
 </script>
+<!-- https://vuetifyjs.com/en/components/forms/ -->
